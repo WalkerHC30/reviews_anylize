7dca8ac67a0077b480b0ca88df697f3ca6231850
WalkerHC <walker.hc30@gmail.com>
2020-06-17 00:04:53 +1000

    add avg

--- r.py
+++ r.py
@@ -4,7 +4,11 @@ with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
-		if count % 1000 == 0:
+		if count % 5000 == 0:
 			print(count)
-print(len(data)) 
-print(data[5])
+print("檔案讀取完畢!總共有", len(data), "筆資料!") 
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print("每筆留言的平均長度是", sum_len / len(data), "個字!")
